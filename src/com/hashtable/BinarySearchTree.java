c62ba30fba852552175acc7e68c4757a904ac901
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-18 03:44:20 +0530

    Added : Finding 63 is present or not in the tree.

--- src/com/hashtable/BinarySearchTree.java
+++ src/com/hashtable/BinarySearchTree.java
@@ -40,11 +40,32 @@ public class BinarySearchTree {
         inOrder(root.right);
     }
 
+    // Uc3 = Search if 63 is present or not in the tree.
+    public static boolean searchNode(Node root, int key){
+        if(root == null) {
+            return false;
+        }
+
+        if (root.data > key){
+            return searchNode(root.left, key);
+        }
+        else if (root.data == key){
+            return true;
+        } else {
+            return searchNode(root.right, key);
+        }
+    }
+
     public static void main(String[] args) {
         insertNode(56);
         insertNode(30);
         insertNode(70);
         inOrder(root);
+
+        if(searchNode(root,63)){
+            System.out.println("63 is present");
+        }else
+            System.out.println("63 is not found");
     }
 
 }
